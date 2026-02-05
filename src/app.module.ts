4063fbffd8038b77a536ffad5744b46647b709c2
nguetcheu <dnguetcheu@gmail.com>
2026-02-05 14:54:42 +0100

    crud method

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
-import { MoviesService } from './services/movies/movies.service';
-import { MoviesModule } from './movies/movies.module';
+import { MoviesService } from './movies/services/movies.service';
 
 @Module({
   imports: [MoviesModule],
