410953dd1765ec77159fa72154840f7e004cd1f1
nguetcheu <dnguetcheu@gmail.com>
2026-02-05 14:54:07 +0100

    Ajout de Movies module, controller, et service avec basic CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { MoviesService } from './services/movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MoviesService],
 })
 export class AppModule {}
