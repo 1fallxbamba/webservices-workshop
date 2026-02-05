97f5f97ab1bb7ff805d34d318c4ed6fa03359e50
nguetcheu <dnguetcheu@gmail.com>
2026-02-05 16:55:47 +0100

    ajout de user DTOs et validation pipes for user creation and updates

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common'; // Ajouté
 import { AppModule } from './app.module';
 
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: false,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT as unknown as number);
 }
 bootstrap();
--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,18 +1,22 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import {
   Body,
   Controller,
   Delete,
   Get,
   HttpCode,
+  HttpException,
+  HttpStatus,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
+import { CreateUserDto } from '../models/create-user.dto';
+import { UpdateUserDto } from '../models/update-user-dto';
 
 @Controller('users')
 export class UsersController {
-  
   @Get()
   @HttpCode(200)
   allUsers() {
@@ -45,19 +49,19 @@ export class UsersController {
 
   @Post()
   @HttpCode(201)
-  createUser(
-    @Body()
-    userData: {
-      username: string;
-      email: string;
-      password?: string;
-    },
-  ) {
-    return {
-      id: 3,
-      ...userData,
-      createdAt: new Date().toISOString(),
-    };
+  createUser(@Body() userData: CreateUserDto) {
+    try {
+      return {
+        id: 3,
+        ...userData,
+        createdAt: new Date().toISOString(),
+      };
+    } catch {
+      throw new HttpException(
+        'Une erreur est survenue lors de la création de l’utilisateur',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Delete(':id')
@@ -71,15 +75,7 @@ export class UsersController {
 
   @Patch(':id')
   @HttpCode(200)
-  patchUser(
-    @Param('id') id: string,
-    @Body()
-    updateData: {
-      username?: string;
-      email?: string;
-      role?: string;
-    },
-  ) {
+  patchUser(@Param('id') id: string, @Body() updateData: UpdateUserDto) {
     return {
       message: `Profil de l'utilisateur ${id} mis à jour`,
       ...updateData,
