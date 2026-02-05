8d94ce6a5f7ad7387e9c4a85a8cfaa5f980d3d3c
uvsq22306626 <benzouzoudouaa28@gmail.com>
2026-02-05 15:22:35 +0100

    update

---
+++ src/movies/controllers/movies.controller.ts
@@ -0,0 +1,60 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+    @Get()
+    @HttpCode(200)
+    allMovies() {
+        return [
+            {
+                id: 1,
+                title: 'Inception',
+                director: 'Christopher Nolan',
+                releaseYear: 2010,
+            },
+            {
+                id: 2,
+                title: 'The Matrix',
+                director: 'Lana Wachowski, Lilly Wachowski',
+                releaseYear: 1999,
+            },
+            {
+                id: 3,
+                title: 'Interstellar',
+                director: 'Christopher Nolan',
+                releaseYear: 2014,
+            }
+        ];
+    }
+    @Get(':id')
+    @HttpCode(200)
+    oneMovieById(@Param('id') id:string){
+        return {
+            id: id,
+            title: 'Inception',
+            director: 'Christopher Nolan',
+            releaseYear: 2010,
+        }
+
+    }
+    @Post()
+    @HttpCode(201)
+    newMovie(@Body() moviesData: {id: number, title: string, director: string, releaseYear: number}){
+        return moviesData;
+    }
+    @Patch(':id')
+    @HttpCode(200)
+    updateMovie(@Param('id') id:string, @Body() newData: {title?: string, director?: string, releaseYear?: number}){
+        return {
+            id: id,
+            ...newData
+        }
+    }
+    @Delete(':id')
+    @HttpCode(204)
+    deleteOneMovie(@Param('id') id:string){
+        return`Movie with id ${id} deleted`
+        
+    }
+
+}
---
+++ src/movies/movies.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MoviesService } from './services/movies.service';
+import { MoviesController } from './controllers/movies.controller';
+
+@Module({
+  providers: [MoviesService],
+  controllers: [MoviesController]
+})
+export class MoviesModule {}
