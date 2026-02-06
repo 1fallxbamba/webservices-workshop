1b309a029eaf9d0cc2b9fbda8915604c65215afd
oumayma djebali <omayma.djebali@gmail.com>
2026-02-06 16:35:29 +0100

    last commit

--- src/partners/partners.module.ts
+++ src/partners/partners.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { PartnersService } from './services/partners.service';
 import { PartnersController } from './controllers/partners.controller';
-
+import { ApiAuthMiddleware } from '../security/api-auth.middleware';
 @Module({
   providers: [PartnersService],
   controllers: [PartnersController]
 })
-export class PartnersModule {}
+export class PartnersModule {
+  configure(consumer: MiddlewareConsumer) {
+  
+      consumer.apply(ApiAuthMiddleware).forRoutes(PartnersController);
+  
+    }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
+
+import { ApiAuthMiddleware } from '../security/api-auth.middleware';
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+
+    consumer.apply(ApiAuthMiddleware).forRoutes(UsersController);
+
+  }
+}
