71bcc04945afd357d3a0d3a9c790e7e802dc1164
nguetcheu <dnguetcheu@gmail.com>
2026-02-06 12:27:55 +0100

    Add middleware restriction

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { MoviesService } from './services/movies.service';
 import { MoviesController } from './controller/movies.controller';
+import { ApiAuthMiddleware } from 'src/security/api-auth.middleware';
 
 @Module({
   providers: [MoviesService],
   controllers: [MoviesController],
 })
-export class MoviesModule {}
+export class MoviesModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiAuthMiddleware).forRoutes(MoviesController);
+  }
+}
--- src/partners/partners.module.ts
+++ src/partners/partners.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { PartnersService } from './services/partners.service';
 import { PartnersController } from './controller/partners.controller';
+import { ApiAuthMiddleware } from 'src/security/api-auth.middleware';
 
 @Module({
   providers: [PartnersService],
-  controllers: [PartnersController]
+  controllers: [PartnersController],
 })
-export class PartnersModule {}
+export class PartnersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ApiAuthMiddleware).forRoutes(PartnersController);
+  }
+}
